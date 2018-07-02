1dcc47c96cf36dd41e614e9661a0463728b5f48a
James Almond <jamestalmond@gmail.com>
2018-07-02 16:35:17 +0100

    Routing, Switching, Params...

--- src/App.js
+++ src/App.js
@@ -1,37 +1,29 @@
 import React, { Component } from 'react';
+import {
+	BrowserRouter as Router,
+	Route,
+	Switch,
+	Link
+} from 'react-router-dom';
 import './App.css';
 
-import Movie from './Movie';
+import MoviesList from './MoviesList';
+import MovieDetail from './MovieDetail';
 
-class App extends Component {
-
-	state = {
-		movies: []
-	}
-	
-	async componentDidMount() {
-		try {
-			const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=0eaff50778a0b2bf2c375f3d8e8755b7&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
-			const movies = await res.json();
-			this.setState({
-				movies: movies.results
-			});
-		} catch(e) {
-			console.log(e);
-		}
-	}
-
-	render() {
-		return (
-			<div className="App">
-				<header className="App-header">
+const App = () => (
+	<Router>
+		<div className="App">
+			<header className="App-header">
+				<Link to="/">
 					<h1>Movie DB</h1>
-				</header>
-				{ this.state.movies.map(movie => <Movie key={ movie.id } movie={ movie } />) }
-			</div>
-		);
-	}
-
-}
+				</Link>
+			</header>
+			<Switch>
+				<Route exact path="/" component={ MoviesList } />
+				<Route path="/:id" component={ MovieDetail } />
+			</Switch>
+		</div>
+	</Router>
+);
 
 export default App;
--- src/Movie.js
+++ src/Movie.js
@@ -1,20 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-export default class Movie extends Component {
-	static propTypes = {
-		movie: PropTypes.shape({
-			title: PropTypes.string.isRequired,
-			overview: PropTypes.string.isRequired
-		})
-	}
+const POSTER_PATH = 'http://image.tmdb.org/t/p/w154';
 
-	render() {
-		return (
-			<div>
-				<h3>{ this.props.movie.title }</h3>
-				<p>{ this.props.movie.overview }</p>
-			</div>
-		)
-	}
+const Movie = ({ movie }) => (
+	<li> 
+		<Link to={ `/${ movie.id }` }>
+			<img src={ `${POSTER_PATH}${movie.poster_path}` } alt={ movie.title } />
+		</Link>
+		<h3>{ movie.title }</h3>
+	</li>
+);
+
+export default Movie;
+
+Movie.propTypes = {
+	movie: PropTypes.shape({
+		title: PropTypes.string.isRequired
+	}).isRequired
 }
